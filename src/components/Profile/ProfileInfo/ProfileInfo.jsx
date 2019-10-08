3455328d9b84b361510d57a5056db5d923aff94d
ArmasPyukkenen <armaspkk@gmail.com>
2019-10-08 22:11:30 +0300

    Added profile wallpaper to resources

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from "./ProfileInfo.module.css";
+import profileWallpaper from "../../../resources/images/profile-wallpaper.jpg";
 
 const ProfileInfo = () => {
     return (
         <div>
             <div>
-                <img className={classes.main_wallpaper} src="https://ru.tokkoro.com/picsup/2955858-mountains-river-snow-forest-dual-monitors-ultra-wide-nature-landscape-digital-art-trees___landscape-nature-wallpapers.jpg" alt="cool stuff"></img>
+                <img className={classes.main_wallpaper} src={profileWallpaper} alt="cool stuff"></img>
             </div>
             <div className={classes.description_block}>
                 ava+desc
