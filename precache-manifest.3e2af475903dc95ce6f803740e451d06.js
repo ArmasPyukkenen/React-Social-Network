9e663d6db05f8ba8ad73ce66d76f6b21a326594b
Armas Pyukkenen <armaspkk@gmail.com>
2021-03-15 05:25:53 +0300

    Updates

---
+++ precache-manifest.3e2af475903dc95ce6f803740e451d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7a7e15c8952a4309f9e8176aee6e94",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "5660f454a426d5350c81",
+    "url": "/react-gh-pages/static/css/main.5cb6d956.chunk.css"
+  },
+  {
+    "revision": "4b3c74dabbcffa7e7f06",
+    "url": "/react-gh-pages/static/js/2.9b20d053.chunk.js"
+  },
+  {
+    "revision": "5660f454a426d5350c81",
+    "url": "/react-gh-pages/static/js/main.479d0b84.chunk.js"
+  },
+  {
+    "revision": "1d886a9804d35ed9e6d0",
+    "url": "/react-gh-pages/static/js/runtime~main.bdb0e5ff.js"
+  },
+  {
+    "revision": "0aa8581c2cb0aa948d63ce3ddad90c81",
+    "url": "/react-gh-pages/static/media/defaultavatar.0aa8581c.png"
+  },
+  {
+    "revision": "96fddef52117d433b1d58dce33ff097a",
+    "url": "/react-gh-pages/static/media/spiderman.96fddef5.jpg"
+  }
+]);
